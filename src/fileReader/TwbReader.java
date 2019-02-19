e1d8a9ec19df68c85bd209488b36e374d517c70c
ML\GA91 <daniel.graves@sunlife.com>
2019-02-19 16:18:03 -0500

    Quick commit

--- src/fileReader/TwbReader.java
+++ src/fileReader/TwbReader.java
@@ -2,10 +2,10 @@ package fileReader;
 
 import java.io.IOException;
 import java.io.File;
-import java.util.Arrays;
 import javax.xml.parsers.*;
 import org.w3c.dom.*;
 import java.io.*;
+
 
 
 public class TwbReader {
@@ -13,15 +13,8 @@ public class TwbReader {
 	static void directoryReader(File theFile, String currentPath) throws IOException {
 		
 		File[] allItems = theFile.listFiles();
-		
-
 		for (File item: allItems) {
 			if(item.isFile() && item.getName().toLowerCase().endsWith(".twb")) {
-				
-				FileInputStream in = null;
-				FileOutputStream out = null;
-				
-				
 				
 				// get the name of the file we're going to use as the input stream
 				String inputFile = item.getParent() + "\\" + item.getName();
@@ -32,11 +25,19 @@ public class TwbReader {
 					Document doc = dBuilder.parse(item);
 					doc.getDocumentElement().normalize();
 					
-					System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-					NodeList nList = doc.getElementsByTagName("calculation");
+					NodeList dsList = doc.getElementsByTagName("datasources");
+					Element dataSources = (Element) dsList.item(0);  // the actual datasources are always the first element
 					
+					NodeList dSources = dataSources.getElementsByTagName("datasource");
 					
-					for (int temp = 0; temp < nList.getLength(); temp++) {
+					for (int i = 0; i < dSources.getLength(); i++) {
+						Element source = (Element) dSources.item(i);
+						NodeList columns = source.getElementsByTagName("column");
+						System.out.print("Datasource: " + source.getAttribute("name"));
+						System.out.println(" with " + columns.getLength() + " columns.");
+					}
+					
+					/*for (int temp = 0; temp < nList.getLength(); temp++) {
 			            Node nNode = nList.item(temp);
 			            System.out.println("\nCurrent Element :" + nNode.getNodeName());
 			            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
@@ -45,48 +46,17 @@ public class TwbReader {
 			               System.out.println(nNode.getParentNode().getAttributes().getNamedItem("name"));
 			               System.out.println(nNode.getParentNode().getAttributes().getNamedItem("caption"));
 			               System.out.println(nNode.getParentNode().getParentNode().getAttributes().getNamedItem("name"));
-			               /*
-			               System.out.println("First Name : " 
+			               
+			               /*System.out.println("First Name : " 
 			                  + eElement
 			                  .getElementsByTagName("firstname")
 			                  .item(0)
 			                  .getTextContent());
-			               System.out.println("Last Name : " 
-			                  + eElement
-			                  .getElementsByTagName("lastname")
-			                  .item(0)
-			                  .getTextContent());
-			               System.out.println("Nick Name : " 
-			                  + eElement
-			                  .getElementsByTagName("nickname")
-			                  .item(0)
-			                  .getTextContent());
-			               System.out.println("Marks : " 
-			                  + eElement
-			                  .getElementsByTagName("marks")
-			                  .item(0)
-			                  .getTextContent());*/
 			            }
-			         }
-					
-					
-					
-					
-				} catch (Exception e) {
-					System.out.println("something went wrong");
-				} finally {
-					
-				}
-				
-				//TODO:  figure out how to get the name for the output stream
-
-				//String[] dirBreakout = item.getParent().split("\\\\", -1);
-				//String[] newarr = Arrays.copyOfRange(dirBreakout,1,dirBreakout.length - 1);
-				//System.out.print(dirBreakout[dirBreakout.length - 1] + "/");
-				//System.out.println(item.getName());
-				
-				//for(String dir: newarr) { System.out.println(dir);}
-				
+			         }	*/				
+				} 
+				catch (Exception e) { System.out.println("something went wrong"); } 
+				finally { }
 			}
 			if(item.isDirectory()) {
 				directoryReader(item, currentPath);
@@ -100,29 +70,8 @@ public class TwbReader {
 		long startTime;
 		long endTime;
 		
-		//FileInputStream in = null;
-		//FileOutputStream out = null;
-		
 		startTime = System.currentTimeMillis();
 		
-		/*try {
-			in = new FileInputStream("c:/dev/book1.twb");
-			out = new FileOutputStream("c:/dev/book1.xml");
-			
-			int c;
-			
-			while((c = in.read()) != -1) {
-				out.write(c);
-			}
-		} finally {
-			if(in != null) {
-				in.close();
-			}
-			if(out != null) {
-				out.close();
-			}
-		}*/
-				
 		File folder = new File("c:/dev");
 		File writedir = new File("c:/dev/dev");
 		
@@ -131,6 +80,9 @@ public class TwbReader {
 		endTime = System.currentTimeMillis();
 		
 		System.out.println(endTime - startTime);
+		
+		DataSource danny = new DataSource();
+
 	}
 
 }
