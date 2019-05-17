ab7b2291f34afbfb23f74e2577f2bc51cbccc0e4
ML\GA91 <daniel.graves@sunlife.com>
2019-05-17 08:43:44 -0400

    committing multiple changes

--- src/fileReader/TwbXMLParser.java
+++ src/fileReader/TwbXMLParser.java
@@ -1,19 +1,19 @@
 package fileReader;
 
-import java.io.File;
+//import java.io.File;
 import java.io.InputStream;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
+//import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 import java.util.*;
 
-@SuppressWarnings("unused")
+//@SuppressWarnings("unused")
 
 public class TwbXMLParser {
 	public static ArrayList<TableauCalculation> parseFile(InputStream iStream) {
@@ -31,20 +31,18 @@ public class TwbXMLParser {
 				if (nList.item(i).getParentNode().getParentNode().getNodeName() == "datasource" && nList.item(i).getAttributes().getNamedItem("formula") != null) {
 					
 					String calcName = nList.item(i).getParentNode().getAttributes().getNamedItem("name").getNodeValue();
-					System.out.println("past calcname");
 					String calcFormula = nList.item(i).getAttributes().getNamedItem("formula").getNodeValue();
-					System.out.println("past formula");
 					String calcCaption = "NA";
+					
 					Node noder = nList.item(i).getParentNode().getAttributes().getNamedItem("caption");
-					if (noder != null) { calcCaption = noder.getNodeValue().toString();}	
-					System.out.println("past caption");				
+					if (noder != null) { calcCaption = noder.getNodeValue().toString();}
 					TableauCalculation tableauCalc = new TableauCalculation("workbook", "version", calcName, calcFormula, calcCaption);
 					
 					tabCalcs.add(tableauCalc);
 					tableauCalc.printCalculation();
 				}
 			}
-			System.out.println("Registered " + tabCalcs.size() + " total calculations in the workbook.");
+			//System.out.println("Registered " + tabCalcs.size() + " total calculations in the workbook.");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
